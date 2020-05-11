5941fb8804d05525733010d407a74332f2039763
bfeldman89 <26087892+bfeldman89@users.noreply.github.com>
2020-05-11 17:57:59 -0500

    Update common.py

--- common.py
+++ common.py
@@ -28,13 +28,15 @@ tw = Twython(os.environ['TWITTER_APP_KEY'], os.environ['TWITTER_APP_SECRET'],
              os.environ['TWITTER_OAUTH_TOKEN'], os.environ['TWITTER_OAUTH_TOKEN_SECRET'])
 
 
-muh_headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
+muh_headers = {
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
 
 
 my_funcs = {'scrape_daily_pop': 'receET4GbZsIHZ1ap',
             'scrape_monthly_fact_scheets': 'recl93NdV0nvxiP6B',
             'scrape_press_releases': 'rec6iaRXOshuZ3OwY',
             'scrape_mdoc_stuff': 'recdI7ExzeoiTYI6A'}
+
 
 def wrap_from_module(module):
     def wrap_it_up(t0, new=None, total=None, function=None):
