6f799436d5d68e1c8f79c318cd60e8dba2dfa89c
bfeldman89 <26087892+bfeldman89@users.noreply.github.com>
2020-07-06 15:45:13 -0500

    Update common.py

--- common.py
+++ common.py
@@ -20,9 +20,6 @@ airtab_tweets = Airtable(base_key=os.environ['botfeldman89_db'],
                          api_key=os.environ['AIRTABLE_API_KEY'])
 
 
-# dc = DocumentCloud(username=os.environ['DOCUMENT_CLOUD_USERNAME'],
-#                    password=os.environ['DOCUMENT_CLOUD_PW'])
-
 dc = DocumentCloud(username=os.environ['MUCKROCK_USERNAME'],
                    password=os.environ['MUCKROCK_PW'])
 
